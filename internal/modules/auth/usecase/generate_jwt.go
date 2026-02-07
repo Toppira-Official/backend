379c893ab8e0bd5c74ced5eb788e990e62a8cf4f
Ali127Hub <AliMoradi0Business@gmail.com>
2026-02-07 20:06:16 +0330

    feat: implement generate jwt usecase

---
+++ internal/modules/auth/usecase/generate_jwt.go
@@ -0,0 +1,44 @@
+package usecase
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/Toppira-Official/backend/internal/configs"
+	apperrors "github.com/Toppira-Official/backend/internal/shared/errors"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type GenerateJwtUsecase interface {
+	Execute(ctx context.Context, id string) (string, error)
+}
+
+type generateJwtUsecase struct {
+	envs configs.Environments
+}
+
+func NewGenerateJwtUsecase() GenerateJwtUsecase {
+	return &generateJwtUsecase{}
+}
+
+func (uc *generateJwtUsecase) Execute(ctx context.Context, id string) (string, error) {
+	expireTimeInHour, err := strconv.Atoi(uc.envs.JWT_EXPIRES_IN_HOURS.String())
+	if err != nil {
+		return "", apperrors.E(apperrors.ErrServerInternalError, err)
+	}
+
+	claims := &jwt.RegisteredClaims{
+		Subject:   id,
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * time.Duration(expireTimeInHour))),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(uc.envs.JWT_SECRET.String()))
+	if err != nil {
+		return "", apperrors.E(apperrors.ErrServerNotResponding, err)
+	}
+
+	return tokenString, nil
+}
